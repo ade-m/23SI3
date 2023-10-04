79a28d87abb21ef5ae62213d32761e2e7c1e69db
ade-m <66313509+ade-m@users.noreply.github.com>
2023-10-04 10:24:36 +0700

    pt 6

---
+++ pertemuan6-1.py
@@ -0,0 +1,43 @@
+# input/ inisialisasi
+n=5
+i=4
+# proses
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print('*',end=' ')
+    print('')
+for i in range(n-1,0,-1):
+    for j in range(1,i+1):
+        print('*',end=' ')
+    print('')
+
+for i in range(1,n+1):
+    for j in range(1,n-i+1):
+        print('*',end=' ')
+    for j in range(1,i+1):
+        print(j,end=' ')
+    print('')
+for i in range(n-1,0,-1):
+    for j in range(1,n-i+1):
+        print('*',end=' ')
+    for j in range(1,i+1):
+        print(j,end=' ')
+    print('')
+
+
+for i in range(1,n+1):
+    for j in range(1,n-i+1):
+        print(' ',end=' ')
+    for j in range(1,i+1):
+        print(j,end=' ')
+    for j in range(i-1,0,-1):
+        print(j,end=' ')
+    print('')
+for i in range(n-1,0,-1):
+    for j in range(1,n-i+1):
+        print(' ',end=' ')
+    for j in range(1,i+1):
+        print(j,end=' ')
+    for j in range(i-1,0,-1):
+        print(j,end=' ')
+    print('')
